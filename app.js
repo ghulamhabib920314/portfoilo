ca4ab52124ea86857c5dae0c1fb89dc6d4151a24
ghulamhabib920314 <gameoover1289@gmail.com>
2024-11-28 12:02:02 +0500

    Portfolio

---
+++ app.js
@@ -0,0 +1,32 @@
+// // Select all skill-box elements
+// const skillBoxes = document.querySelectorAll('.skill-box');
+
+// // Add hover effect for each skill box
+// skillBoxes.forEach(box => {
+//   box.addEventListener('mouseover', function() {
+//     const percentage = this.getAttribute('data-skill');
+//     this.querySelector('.percentage').textContent = percentage;
+//   });
+// });
+
+// // Function to show elements when they scroll into view
+// function showOnScroll() {
+//   const leftSection = document.querySelector('.left');
+//   const triggerPoint = window.innerHeight / 1.2; // Trigger when it's 80% in view
+
+//   const leftPosition = leftSection.getBoundingClientRect().top;
+
+//   if (leftPosition < triggerPoint) {
+//       leftSection.classList.add('show'); // Add the show class to trigger the animation
+//   }
+// }
+
+// // Event listener for scroll
+// window.addEventListener('scroll', showOnScroll);
+
+// // Initialize scroll effect on page load
+// document.addEventListener('DOMContentLoaded', showOnScroll);
+
+
+
+
